fd31ea066afdf8f64ac52a54aed30143ddd1604e
Oded Leiba <oded@colu.com>
2019-05-16 15:54:47 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,140 @@
+import {Party2, Party2Share, Signature} from '@kzen/thresh-sig';
+import 'babel-polyfill';
+const bncClient = require('@binance-chain/javascript-sdk');
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
+
+const FE_BYTES_SIZE = 32;
+const GOTHAM_ENDPOINT = 'http://localhost:8000';
+const HD_COIN_INDEX = 0;
+const PARTY2_SHARE_PATH = path.join(__dirname, '../../p2-share.json');
+const BINANCE_CHAIN_URL_MAINNET = 'https://dex.binance.org/';
+const BINANCE_CHAIN_URL_TESTNET = 'https://testnet-dex.binance.org';
+
+export class BncThreshSigClient {
+    private p2: Party2;
+    private p2MasterKeyShare: Party2Share;
+    private bncClient: any;
+    private addressesToIndexes: {[address: string]: number};
+
+    constructor(mainnet: boolean = false, useAsyncBroadcast: boolean = false) {
+        const url = mainnet ? BINANCE_CHAIN_URL_MAINNET : BINANCE_CHAIN_URL_TESTNET;
+        this.bncClient = new bncClient(url, useAsyncBroadcast);
+        this.p2 = new Party2(GOTHAM_ENDPOINT);
+        this.bncClient.setSigningDelegate(this.sign.bind(this));
+        this.addressesToIndexes = {};
+    }
+
+    /**
+     * Initialize the client's chain ID
+     * @return {Promise}
+     */
+    public async initChain() {
+        return this.bncClient.initChain();
+    }
+
+    /**
+     * Initialize the client's master key.
+     * Will either generate a new one by the 2 party protocol, or restore one from previous session.
+     * @return {Promise}
+     */
+    public async initMasterKey() {
+        this.p2MasterKeyShare = await this.restoreOrGenerateMasterKey();
+    }
+
+    /**
+     * get the address of the specified index. If the index is omitted, will return the default address (of index 0).
+     * @param addressIndex HD index of the address to get
+     */
+    public getAddress(addressIndex?: number): string {
+        addressIndex = addressIndex || 0;
+        const publicKey = this.getPublicKey(addressIndex);
+        const publicKeyHex = publicKey.encode('hex', true);
+        const address = bncClient.crypto.getAddressFromPublicKey(publicKeyHex);
+        this.addressesToIndexes[address] = addressIndex;
+        return address;
+    }
+
+    /**
+     * Transfer tokens from one address to another.
+     * @param {String} fromAddress
+     * @param {String} toAddress
+     * @param {Number} amount
+     * @param {String} asset
+     * @param {String} memo optional memo
+     * @param {Number} sequence optional sequence
+     * @return {Promise} resolves with response (success or fail)
+     */
+    public async transfer(fromAddress: string, toAddress: string, amount: number, asset: string, memo='', sequence=null) {
+        return this.bncClient.transfer(fromAddress, toAddress, amount, asset, memo, sequence);
+    }
+
+    /**
+     * get account of specified address. If address is omitted, will use the default address (of index 0).
+     * @param {String} address
+     * @return {Promise} resolves with http response
+     */
+    public async getAccount(address?: string) {
+        if (!address) {
+            address = this.getAddress(0);
+        }
+        return this.bncClient.getAccount(address);
+    }
+
+    /**
+     * get balances of the specified address. If no address specified, will use the default address (of index 0).
+     * @param {String} address optional address
+     * @return {Promise} resolves with http response
+     */
+    public async getBalance(address?: string) {
+        if (!address) {
+            address = this.getAddress(0);
+        }
+        return this.bncClient.getBalance(address);
+    }
+
+    /**
+     * The signing delegate which uses the local master key share and performs 2P-ECDSA with party one's server.
+     * @param  {Transaction} tx      the transaction
+     * @param  {Object}      signMsg the canonical sign bytes for the msg
+     * @return {Transaction}
+     */
+    private async sign(tx: any, signMsg: any) {
+        const fromAddress: string = (signMsg.inputs && signMsg.inputs[0].address) || signMsg.sender;
+        const addressIndex: number = this.addressesToIndexes[fromAddress];
+        const signBytes: Buffer = tx.getSignBytes(signMsg);
+        const p2ChildShare: Party2Share = this.p2.getChildShare(this.p2MasterKeyShare, HD_COIN_INDEX, addressIndex);
+        const msgHash: Buffer = crypto.createHash('sha256').update(signBytes).digest();
+        const signature: Signature = await this.p2.sign(msgHash, p2ChildShare, HD_COIN_INDEX, addressIndex);
+        const publicKey = this.getPublicKey(addressIndex);
+        tx.addSignature(publicKey, signature.toBuffer());
+        return tx;
+    }
+
+    /**
+     * @return {Elliptic.PublicKey} PubKey
+     */
+    private getPublicKey(addressIndex: number) {
+        // assuming a single default address
+        const p2ChildShare = this.p2.getChildShare(this.p2MasterKeyShare, HD_COIN_INDEX, addressIndex);
+        return p2ChildShare.getPublicKey();
+    }
+
+    private async restoreOrGenerateMasterKey(): Promise<Party2Share> {
+        if (fs.existsSync(PARTY2_SHARE_PATH)) {
+            let p2MasterKeyShare: string = fs.readFileSync(PARTY2_SHARE_PATH, 'utf8');
+            if (p2MasterKeyShare) {
+                return JSON.parse(p2MasterKeyShare);
+            }
+        }
+
+        return this.generateMasterKeyShare();
+    }
+
+    private async generateMasterKeyShare(): Promise<Party2Share> {
+        const p2MasterKeyShare: Party2Share = await this.p2.generateMasterKey();
+        fs.writeFileSync(PARTY2_SHARE_PATH, JSON.stringify(p2MasterKeyShare));
+        return p2MasterKeyShare;
+    }
+}
---
+++ test/client-tests.js
@@ -0,0 +1,91 @@
+const BncThreshSigClient = require('../dist/src').BncThreshSigClient;
+const expect = require('chai').expect;
+const BnbApiClient = require('@binance-chain/javascript-sdk');
+
+describe('Binance client tests (make sure to deposit to printed address)', () => {
+    let bncThreshSigClient;
+
+    before(async () => {
+        console.error = () => {};  // suppress error logs in test to keep it clean
+        bncThreshSigClient = new BncThreshSigClient();
+        await bncThreshSigClient.initChain();
+        await bncThreshSigClient.initMasterKey();
+    });
+
+    it('get address', () => {
+        const address = bncThreshSigClient.getAddress();
+        expect(address).to.be.a('string');
+        console.log(address);
+    });
+
+    it('get address without address index should return same address', () => {
+        const address1 = bncThreshSigClient.getAddress();
+        const address2 = bncThreshSigClient.getAddress();
+        expect(address1).to.be.a('string');
+        expect(address2).to.be.a('string');
+        expect(address1).to.equal(address2);
+    });
+
+    it('get address without address index should return default address (of index 0)', () => {
+        const address1 = bncThreshSigClient.getAddress();
+        const address2 = bncThreshSigClient.getAddress(0);
+        expect(address1).to.be.a('string');
+        expect(address2).to.be.a('string');
+        expect(address1).to.equal(address2);
+    });
+
+    it('get address with same address index should return same address', () => {
+        const address1 = bncThreshSigClient.getAddress(7);
+        const address2 = bncThreshSigClient.getAddress(7);
+        expect(address1).to.be.a('string');
+        expect(address2).to.be.a('string');
+        expect(address1).to.equal(address2);
+    });
+
+    it('get address with different address index should return different address', () => {
+        const address1 = bncThreshSigClient.getAddress(0);
+        const address2 = bncThreshSigClient.getAddress(7);
+        expect(address1).to.be.a('string');
+        expect(address2).to.be.a('string');
+        expect(address1).to.not.equal(address2);
+    });
+
+    it('get account for address', async () => {
+        const address = bncThreshSigClient.getAddress();
+        const account = await bncThreshSigClient.getAccount(address);
+        expect(account).to.be.an('object');
+        expect(account.status).to.eq(200);
+        expect(account.result).to.be.an('object');
+        expect(account.result.address).to.eq(address);
+        expect(account.result.account_number).to.be.a('number');
+        expect(account.result.balances).to.be.an('array');
+        expect(Buffer.from(account.result.public_key).toString('hex').length).to.eq(66);
+    });
+
+    it('get balance for address', async () => {
+        const address = bncThreshSigClient.getAddress();
+        const balance = await bncThreshSigClient.getBalance(address);
+        expect(balance).to.be.an('array');
+        expect(balance.length).to.eq(1);
+        expect(balance[0].symbol).to.eq('BNB');
+        expect(balance[0].free).to.be.a('string');
+        expect(balance[0].frozen).to.be.a('string');
+        expect(balance[0].locked).to.be.a('string');
+    });
+
+    it('transfer', async () => {
+        const addressFrom = bncThreshSigClient.getAddress();
+        const addressTo = 'tbnb1fcczqjxk7hq5fnzyp79vym6e3565ktkh5gy5fw';
+        const amount = 0.00001;
+        const asset = 'BNB';
+        const message = 'take some back';
+        const transferResponse = await bncThreshSigClient.transfer(addressFrom, addressTo, amount, asset, message);
+        expect(transferResponse).to.be.an('object');
+        expect(transferResponse.status).to.eq(200);
+        expect(transferResponse.result).to.be.an('array');
+        expect(transferResponse.result.length).to.be.eq(1);
+        expect(transferResponse.result[0].code).to.be.a('number');
+        expect(transferResponse.result[0].hash).to.be.a('string');
+        expect(transferResponse.result[0].ok).to.eq(true);
+    });
+});
